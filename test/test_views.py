4a4e8c8cf8c02cb8ef45b7727fc3da4a93cedb6d
Lucynaaa <33197568+Lucynaaa@users.noreply.github.com>
2020-10-06 15:17:51 +0200

    Create test_views.py

---
+++ test/test_views.py
@@ -0,0 +1,11 @@
+import unittest
+from hello_world import app
+
+class FlaskrTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_msg_with_output(self):
+        response = self.app.get('/')
+        self.assertEqual(b"Hello: Lucyna", response.data)
